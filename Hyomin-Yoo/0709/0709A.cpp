cd4b8232090e4b6bfc7d27275c29571a0fb7dcc3
hyomin <hmybjm95@naver.com>
2019-07-09 14:06:26 +0900

    0709

---
+++ Hyomin-Yoo/0709/0709A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class MyClass{
+	MyClass(){
+		cout << "생성자 호출"<<endl;
+	}
+	~MyClass(){
+		cout <<"소멸자 호출"<<endl;
+	}
+	void PrintThis(){
+		cout << this << endl;
+	}
+}
+
+int main(){
+	MyClass a,b;
+
+	a.PrintThis();
+}
---
+++ Hyomin-Yoo/0709/0709Q.cpp
@@ -0,0 +1,17 @@
+MyClass를 완성하여 생성자, 소멸자의 호출을 확인시기를 확인하고 PrintThis함수로 a의 주소를 출력한다. 
+
+#include <iostream>
+
+using namespace std;
+
+class MyClass{
+	void PrintThis(){
+		
+	}
+}
+
+int main(){
+	MyClass a,b;
+
+	a.PrintThis();
+}

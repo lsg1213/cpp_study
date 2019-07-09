f5006a846a2285ecffa5592d084ea0a3dcfceb8d
Seungjin Lee <lsg0385@naver.com>
2019-07-09 18:17:49 +0900

    190709 end of study

---
+++ Seungjin-Lee/190709/problem_constructor.cpp
@@ -0,0 +1,36 @@
+// constructor, destructor
+// (1)  how many destructors are printed after main?
+// (2)  please predict constructor and destructor order !!  
+
+#include<iostream>
+using namespace std;
+
+class Myclass{	
+
+public:
+	Myclass(){
+		cout << "constructor called for " << this << endl;
+	}
+	~Myclass(){
+		cout << "destructor called for " << this << endl;
+	}
+
+};
+
+Myclass d;
+
+int main(){
+	cout << "main start" << endl;
+	
+	Myclass a;
+	{Myclass b;}
+	
+	if(1){
+	Myclass c;
+	}
+
+	cout<< "main end" << endl;
+	
+}
+
+

da681ed4ffed4ebfed403a9a69add8e124cf97c3
Seungjin Lee <lsg0385@naver.com>
2019-07-13 13:48:13 +0900

    190712 study

--- Changwon-Lee/190709/190709q.cpp
+++ Changwon-Lee/190709/190709q.cpp
@@ -4,37 +4,37 @@ using namespace std;
 
 class chin{
 private:
-	char c;
-	int i;
+   char c;
+   int i;
 
 public:
-	//c = 'a'로 i = 15로 초기화 해주는 생성자를 작성하시오. 
-	
-	void show(){
-		cout << "c : " << c << endl;
-		cout << "i : " << i << endl;
-	}
+   //c = 'a'로 i = 15로 초기화 해주는 생성자를 작성하시오. 
+   chin() : c = 'a', i = 15{}
+   void show(){
+      cout << "c : " << c << endl;
+      cout << "i : " << i << endl;
+   }
 };
  
 
 int main(){
-	chin a;
-	
-	//class chin a의 크기를 출력하시오 
-	cout << "sizeof(a) : " << sizeof(a) << endl;
-	
-	a.show();
-	
-	
-	//접근제한자를 고려하지 말고 포인터를 통하여 
-	//a의 멤버를 각각 c = 'b'로 i = 20으로 바꾸어라 
+   chin a;
+   
+   //class chin a의 크기를 출력하시오 
+   cout << "sizeof(a) : " << sizeof(a) << endl;
+   
+   a.show();
+   
+   
+   //접근제한자를 고려하지 말고 포인터를 통하여 
+   //a의 멤버를 각각 c = 'b'로 i = 20으로 바꾸어라 
 
-	
-	//해설을 위한 값
-	
-	printf("%d\n", p);
-	printf("%d\n", p+4);
-	
-	//
-	a.show();
+   
+   //해설을 위한 값
+   
+   printf("%d\n", p);
+   printf("%d\n", p+4);
+   
+   //
+   a.show();
 }

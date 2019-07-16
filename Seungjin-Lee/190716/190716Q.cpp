878b1202b6255500380e5b98c003320a1c8f3f49
Seungjin Lee <lsg0385@naver.com>
2019-07-16 15:32:37 +0900

    190716 solution

---
+++ Seungjin-Lee/190716/190716Q.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+//divide definition and declaration
+
+using namespace std;
+
+class Vector{
+private:
+	float x;
+	float y;
+	
+public:
+	Vector();
+	Vector(float x, float y);
+	float GetX() const;
+	float GetY() const;
+	Vector Add(Vector b);
+	static Vector Sum(Vector a, Vector b);
+}; 
+Vector::Vector():x(0), y(0) {}
+Vector::Vector(float x, float y) : x(x), y(y) {}
+float Vector::GetX() const {return x;}
+float Vector::GetY() const {return y;}
+Vector Vector::Add(Vector b) { return Vector(x + b.GetX(), y + b.GetY());}
+Vector Vector::Sum(Vector a, Vector b) {return Vector(a.GetX() + b.GetX(), a.GetY() + b.GetY());}
+
+int main(){
+	Vector a(2,3);
+	Vector b(-1,4);
+	Vector c = Vector::Sum(a,b);
+	Vector d = a.Add(b);
+	 
+	cout << a.GetX() << " " << a.GetY() << endl;
+	cout << b.GetX() << " " << b.GetY() << endl;
+	cout << c.GetX() << " " << c.GetY() << endl;
+	cout << d.GetX() << " " << d.GetY() << endl;
+	
+	return 0;
+} 

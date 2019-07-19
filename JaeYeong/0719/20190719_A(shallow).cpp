05e2c23ed871f25f023f4df5b8c006f3520071bf
jyfregramer <38378168+jyfregramer@users.noreply.github.com>
2019-07-19 14:16:26 +0900

    0719

---
+++ JaeYeong/0719/20190719_A(shallow).cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Member
+{
+public:
+    int age;
+    char* name;
+
+public:
+    Member(int _age, char* _name) //Constructor
+    {
+        age = _age;
+        name = new char [strlen(_name)+1];
+        strcpy(name, _name);
+    }
+/*
+    Member(const Member& s) //Constructor : Deep copy
+    {
+        age = s.age;
+        name = new char [strlen(s.name+1];
+        strcpy(name, s.name);
+    }
+*/
+
+    Member(const Member& s) // Constructor : Shallow copy
+    {
+        age = s.age;
+        name = s.name;
+    }
+
+    ~Member() {delete name;}
+
+    void printMember()
+    {
+        cout << "name: " << name << endl;
+        cout << "age: " << age << endl;
+    }
+};
+
+int main()
+{
+    Member Q(23, "Mr.ABc");
+    Member W = Q;
+
+    Q.age = 24;
+    strcpy(Q.name, "Mr.QWc");
+
+    Q.printMember();
+    W.printMember();
+
+    return 0;
+}

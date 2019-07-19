bf3cf00d5a1cac98ec71f3acbcced365bd9ce3a4
SwimLee <syleemk@gmail.com>
2019-07-19 10:42:56 +0900

    190719 prob add

---
+++ Sooyoung-Lee/190720/sooyoungP0719.cpp
@@ -0,0 +1,35 @@
+/*
+	아래의 코드는 2차원 배열 mat[row][col]을 동적 할당받아서 초기화 하는 예이다.
+*/
+
+
+#include <iostream>
+int main()
+{
+	int col, row;
+	std::cin >> row >> col;
+
+	// 1. 2차원 배열을 동적할당 하세요 (for문 사용)
+	int **mat; 
+
+
+	// 2차원 배열을 초기화하고 출력
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			mat[i][j] = i * col + j;
+		}
+	}
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			std::cout << mat[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+
+	// 2. 동적할당 받은 2차원 배열을 해제하세요 (for문 사용)
+	
+}

7801c29db4db8996b22d2519a13e65061950e925
SwimLee <syleemk@gmail.com>
2019-07-22 14:10:57 +0900

    sol upload

---
+++ Sooyoung-Lee/190716/joona0716.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+/* 1. const 붙일수 있는 곳에 다 붙여보기 */
+class Op {
+public:
+	/* 2. 생성자의 선언과 정의 분리하기(밑에 내용과 동일한 결과 나오게 수정) */
+	Op();
+	Op(const int a,const int b,const int c);
+
+	float getA();
+	float getB();
+	float getC();
+
+	/* 3. 덧셈, 뺄셈은 a끼리, b끼리, c끼리 연산.
+	곱셈, 나눗셈은 a, b, c에 입력된 숫자가 각각 연산.
+	멤버 메서드의 선언, 정의 분리해서 클래스 밖에서 정의하기*/
+	Op operator+(Op);
+	Op operator-(Op);
+	Op operator*(float);
+	Op operator/(float);
+
+
+	const static Op Sum(const Op x,const Op y) {
+		return Op(x.a + y.a, x.b + y.b, x.c + y.c);
+	}
+
+
+private:
+	int a;
+	int b;
+	int c;
+};
+
+Op::Op() :a(0), b(0), c(0) { };
+Op::Op(const int a, const int b, const int c) : a(a), b(b), c(c) { };
+
+float Op::getA() { return a; }
+float Op::getB() { return b; }
+float Op::getC() { return c; }
+
+Op Op::operator+(Op x) {
+	return Op(a + x.a, b + x.b, c + x.c);
+}
+Op Op::operator-(Op x) {
+	return Op(a - x.a, b - x.b, c - x.c);
+}
+Op Op::operator*(float x) {
+	return Op(a * x, b * x, c * x);
+}
+Op Op::operator/(float x) {
+	return Op(a / x, b / x, c / x);
+}
+
+int main() {
+	Op x(3, 6, 9), y(2, 4, 6);
+	Op z1 = x + y;
+	Op z2 = x - y;
+	Op z3 = x * 5;
+	Op z4 = y / 2;
+	/* 4. 정적 메서드 사용하여 z1과 동일한 결과나오는지 확인*/
+	Op z5 = Op::Sum(x,y);
+
+	cout << z1.getA() << ", " << z1.getB() << ", " << z1.getC() << endl;
+	cout << z2.getA() << ", " << z2.getB() << ", " << z2.getC() << endl;
+	cout << z3.getA() << ", " << z3.getB() << ", " << z3.getC() << endl;
+	cout << z4.getA() << ", " << z4.getB() << ", " << z4.getC() << endl;
+	cout << z5.getA() << ", " << z5.getB() << ", " << z5.getC() << endl;
+
+	return 0;
+}
---
+++ Sooyoung-Lee/190716/sungmoon0716.cpp
@@ -0,0 +1,160 @@
+/* *
+ * DO NOT use other libraries!!! such as <string.h> or <cstring>
+ * */
+
+#include <iostream>
+using namespace std;
+
+class MyString {
+
+friend ostream& operator<< (ostream &os, const MyString& myString);
+
+private:
+    static int cnt;
+    int id;
+    char *ptrStr;
+    size_t length;
+
+public:
+    /* *
+     * Constructor
+     * You can create an instance like below expressions.
+     * ex)
+     *   MyString s1("Hello");
+     *   MyString s2 = MyString("Hello");
+     *   MyString s3 = "Hello";
+     *   MyString *s4 = new MyString("Hello");  <- dynamic allocation
+     * */
+    MyString (const char *str) : id(cnt++) {
+        size_t size = 0;
+        while (str[size++] != '\0') {}
+
+        ptrStr = new char[size];
+        for (int i = 0; i < size; i++) {
+            ptrStr[i] = str[i];
+        }
+
+        length = size - 1;
+
+        // For logging
+        cout << "Normal Constructor is called. ";
+        cout << "\"" << ptrStr << "\" is created! (ID: " << id << ")" << endl;
+    };
+
+    /* *
+     * Copy Constructor (Deep Copy)
+     * User can use below syntax.
+     * ex)
+     *   MyString s1 = "Hello";
+     *   MyString s2(s1);
+     *   ( s2 == "Hello" )
+     * */
+    MyString (const MyString &myString) : id(cnt++) {
+        length = myString.length;
+        ptrStr = new char[length + 1];
+
+        for (int i = 0; i < length + 1; i++) {
+            ptrStr[i] = myString.ptrStr[i];
+        }
+
+        // For logging
+        cout << "Copy Constructor is called. ";
+        cout << "\"" << ptrStr << "\" is created! (ID: " << id << ")" << endl;
+    }
+
+    // Destructor
+    ~MyString() {
+        delete[] ptrStr;
+
+        // For logging
+        cout << "Destructor is called. ";
+        cout << "\"" << ptrStr << "\" is deleted! (ID: " << id << ")" << endl;
+    }
+
+    /* * 
+     *  User can access and modify characters of this instance
+     *  using this method.
+     *  ex)
+     *    MyString s1 = "Hello";
+     *    s1[3] = '2';
+     *    ( s1 == "Hel2o")
+     * */
+    char& operator[] (int index) {
+        return ptrStr[index];
+    };
+
+    /* *
+     *  Implementation of string concantenation.
+     * */
+    MyString operator+ (const MyString& str) {
+        char *newStr = new char[length + str.length + 1];
+        
+        size_t i = 0;
+        size_t j = 0;
+
+        while(ptrStr[i] != '\0') {
+            newStr[i + j] = ptrStr[i];
+            i++;
+        }
+
+        while (str.ptrStr[j] != '\0') {
+            newStr[i + j] = str.ptrStr[j];
+            j++;
+        }
+        newStr[i + j] = '\0';
+
+        MyString newMyString(newStr);
+        delete[] newStr;
+
+        return newMyString;
+    };
+
+    /* *
+     *  Implementation of string repetition.
+     *  ex) 
+     *    MyString s1 = "HI";
+     *    MyString s2 = s1 * 4;
+     *    ( s2 == "HIHIHIHI" )
+     * */
+    MyString operator* (int num) {
+        char *newStr = new char[length*num + 1];
+
+        for (int i = 0; i < num; i++) {
+            for (size_t j = 0; j < length; j++) {
+                newStr[i*length + j] = ptrStr[j];
+            }
+        }
+        newStr[length*num + 1] = '\0';
+
+        MyString newMyString(newStr);
+        delete[] newStr;
+
+        return newMyString;
+    };
+};
+int MyString::cnt = 1;
+
+/* *
+ * Overload the operator "<<" to use statment like 'cout << myString'.
+ * */
+ostream& operator<< (ostream &os, const MyString& myString) {
+    cout << myString.ptrStr;
+    return os;
+}
+
+int main() {
+    MyString s1 = "snow";
+    MyString s2(s1);
+
+    s2[0] = 'k';
+    
+    MyString s3 = s1 + s2;
+    MyString s4 = s3 * 3;
+
+    cout << s4 << endl;
+    cout << s3 << endl;
+    cout << s2 << endl;
+    cout << s1 << endl;
+
+    return 0;
+}

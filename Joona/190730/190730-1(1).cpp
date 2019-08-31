d18bf44ed1212047ed3e631af99e4c8b063fa6e9
joonachoi <joona95@naver.com>
2019-08-31 11:37:15 +0900

    20190730

---
+++ Joona/190730/190730-1(1).cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//같이 주어진 input.txt를 확인하여, output.txt파일의 내용물로 암호화 시켜줄 수 있도록 하여라.
+//간단한 암호화 과정에서는 간단한 비트연산자만이 사용되었다.
+//hint! , 값을 printf %d와 ascii code를 통해서 어떤 비트연산자가 사용되었는지 간단하게 알 수 있다.
+//깔끔하게 값을 출력해내기 위해서 처리해야할 조건이 무엇인지 확인하라.
+//본과정의 요구사항은 다음과 같다. 
+//같이 주어진 input.txt를 확인하여, output.txt파일의 내용물로 암호화 시켜줄 수 있도록 하여라.
+
+int main(void) {
+	FILE* in, * out;
+	unsigned char n;
+
+	in = fopen("input.txt", "r");
+	out = fopen("output.txt", "a+");
+
+	while (fscanf(in,"%c", &n)!=EOF) {
+		/**/
+		n = ~n;
+		fprintf(out, "%d\n", n);
+	}
+
+	fclose(in);
+	fclose(out);
+
+	return 0;
+}
